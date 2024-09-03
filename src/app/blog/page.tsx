2d2707286452096c6d357186af4770321a7a2447
ismail <ismailismo78@gmail.com>
2024-09-03 19:23:31 +0100

    refactor: Update page components with loading spinners

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,11 @@
-import { GeistSans } from "geist/font/sans";
 import React from "react";
 import { ImSpinner9 } from "react-icons/im";
 
 function Blog() {
 	return (
-		<main className="container min-h-[83vh] font-bold text-[24px] sm:text-[32px] flex justify-center items-center">
+		<main className="container min-h-[90vh] font-bold text-[24px] sm:text-[32px] flex justify-center items-center">
 			<div className="flex gap-4 justify-center items-center">
-				<h1 className={`${GeistSans.className}`}>Under Development </h1>
+				<h1>Under Development </h1>
 				<ImSpinner9 className="text-xl md:text-2xl animate-spin" />
 			</div>
 		</main>
