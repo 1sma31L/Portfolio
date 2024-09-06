bd0e389216957c2fd12178384320264bbb607fc7
ismail <ismailismo78@gmail.com>
2024-09-06 16:04:54 +0100

    style: Updated the footer style and made the code more readable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,31 +2,35 @@ import Link from "next/link";
 import React from "react";
 
 function Footer() {
+	const links = [
+		{ name: "About", href: "/" },
+		{ name: "Projects", href: "/projects" },
+		{ name: "Blog", href: "/blog" },
+		{ name: "Contact", href: "/contact/#header" },
+	];
 	return (
 		<footer
-			className={`container mx-auto h-[15vh]  rounded-lg text-xs md:text-sm flex justify-center items-center p-4 md:px-0 flex-col`}>
-			<div className="flex flex-col sm:flex-row gap-2 justify-between items-center w-full ">
-				<div className="dark:border-white no-underline duration-300 ease-in ">
-					<p className=" text-center">
-						Contact me at{" "}
-						<Link href={"mailto:im.boussekine@gmail.com"} className="font-bold">
-							im.boussekine@gmail.com
-						</Link>
-					</p>
-				</div>
+			className={`container mx-auto h-[15vh]  rounded-lg text-xs md:text-sm flex justify-center items-center p-4 md:px-0 flex-col `}>
+			<div className="flex flex-col sm:flex-row gap-2 justify-between items-center w-full border-2 p-4 border-black">
+				<p className=" text-center">
+					Contact me at{" "}
+					<Link
+						href={"mailto:im.boussekine@gmail.com"}
+						className="font-bold hover:underline">
+						im.boussekine@gmail.com
+					</Link>
+				</p>
 				<div className="flex gap-2 ">
-					<Link href="/" className="hover:underline">
-						About
-					</Link>
-					<Link href="/projects" className="hover:underline">
-						Projects
-					</Link>
-					<Link href="/blog" className="hover:underline">
-						Blog
-					</Link>
-					<Link href="/contact/#header" className="hover:underline">
-						Contact
-					</Link>
+					{links.map((link) => {
+						return (
+							<Link
+								key={link.name}
+								href={link.href}
+								className="hover:underline">
+								{link.name}
+							</Link>
+						);
+					})}
 				</div>
 			</div>
 		</footer>
