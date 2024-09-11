cbf85070552ce4eb06a56acec18763a85987433c
ismail <ismailismo78@gmail.com>
2024-09-11 17:06:06 +0100

    refactor: Update environment variables configuration
    Refactor the code to update the configuration of environment variables in the `next.config.mjs` and `src/config/firebase.ts` files. Instead of hardcoding the values, the code now retrieves the values from the environment variables using the `dotenv` package. This improves security and allows for easier configuration management.
    
    Also, update the `src/lib/sendEmail.ts` file to handle cases where the required environment variables are not defined. If the `PUBLIC_KEY`, `SERVICE_ID`, or `TEMPLATE_ID` environment variables are not found, an error message is logged to the console.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from "dotenv";
+dotenv.config();
+const nextConfig = {
+	env: {
+		API_KEY: process.env.API_KEY,
+		AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+		PROJECT_ID: process.env.PROJECT_ID,
+		STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+		MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+		APP_ID: process.env.APP_ID,
+		MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+		PUBLIC_KEY: process.env.PUBLIC_KEY,
+		SERVICE_ID: process.env.SERVICE_ID,
+		TEMPLATE_ID: process.env.TEMPLATE_ID,
+	},
+};
 
 export default nextConfig;
--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -2,17 +2,29 @@ import emailjs from "@emailjs/browser";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Initialize EmailJS with the public key
-emailjs.init(process.env.NEXT_PUBLIC_PUBLIC_KEY!);
+if (process.env.PUBLIC_KEY !== undefined) {
+	emailjs.init(process.env.PUBLIC_KEY);
+} else {
+	console.error("Error initializing EmailJS: Public key not found");
+}
 
 export const sendEmail = async (templateParams: any) => {
 	try {
-		const response = await emailjs.send(
-			process.env.NEXT_PUBLIC_SERVICE_ID!,
-			process.env.NEXT_PUBLIC_TEMPLATE_ID!,
-			templateParams
-		);
-		console.log("Email sent successfully:", response);
+		if (
+			process.env.SERVICE_ID !== undefined &&
+			process.env.TEMPLATE_ID !== undefined
+		) {
+			const response = await emailjs.send(
+				process.env.SERVICE_ID,
+				process.env.TEMPLATE_ID,
+				templateParams
+			);
+			console.log("Email sent successfully:", response);
+		} else {
+			console.error(
+				"Error sending email: Problem with EmailJS configuration (service or template ID not found)"
+			);
+		}
 	} catch (error: any) {
 		console.error("Error sending email:", error.response || error.message);
 	}
