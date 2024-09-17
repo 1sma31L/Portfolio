e4a3ee5546be33d18c40bd5a4ba447d67cd027c3
ismail <ismailismo78@gmail.com>
2024-09-17 17:02:07 +0100

    style: Update progress bar height and color in ProgressBar component

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -17,9 +17,9 @@ export default function ProgressBar() {
 	}, []);
 
 	return (
-		<div className="fixed top-[7vh] left-0 w-full h-[5px] bg-transparent z-[2000]">
+		<div className="fixed top-[7.2vh] left-0 w-full h-[2px] bg-transparent z-10">
 			<div
-				className="h-full bg-purple-500/75 dark:bg-purple-500/75 transition-all duration-300 ease-out"
+				className="h-full bg-purple-500 transition-all duration-300 ease-out"
 				style={{ width: `${scrollProgress}%` }}></div>
 		</div>
 	);
