e73c08f3b0cebfda3dce770d09dc5bcbce9ebc98
ismail <ismailismo78@gmail.com>
2024-09-17 13:49:28 +0100

    style: Update selection color and progress bar animation

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -2,8 +2,11 @@
 
 import { useEffect, useState } from "react";
 
+import { motion } from "framer-motion";
+
 export default function ProgressBar() {
 	const [scrollProgress, setScrollProgress] = useState(0);
+
 	const handleScroll = () => {
 		const totalHeight = document.body.scrollHeight - window.innerHeight;
 		const scrollPosition = window.scrollY;
@@ -17,10 +20,14 @@ export default function ProgressBar() {
 	}, []);
 
 	return (
-		<div className="fixed top-0 left-0 w-full h-1 bg-transparent z-50">
-			<div
-				className="h-full bg-black dark:bg-white"
-				style={{ width: `${scrollProgress}%` }}></div>
+		<div className="fixed top-0 left-0 w-full h-[0.4rem] bg-transparent z-50">
+			<motion.div
+				className="h-full bg-purple-500"
+				style={{ width: `${scrollProgress}%` }}
+				initial={{ width: 0 }}
+				animate={{ width: `${scrollProgress}%` }}
+				transition={{ ease: "easeOut", duration: 0.3 }}
+			/>
 		</div>
 	);
 }
