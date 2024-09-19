088e9f49c16670f93f42bb91dfa87c0ead610383
ismail <ismailismo78@gmail.com>
2024-09-20 00:37:04 +0100

    refactor: used background var() instead of hard typing colors

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -18,14 +18,14 @@ function InfiniteScroll({ icons }: { icons: TItem[] }) {
 			className="flex w-full overflow-hidden whitespace-nowrap
       relative bg-zinc-50 dark:bg-zinc-950 
   before:absolute before:left-0 before:top-0 before:z-[2] before:h-full md:before:w-[80px] before:w-[30px] 
-  before:bg-[linear-gradient(to_right,theme(colors.zinc.50)_0%,rgba(255,255,255,0)_100%)] 
-  dark:before:bg-[linear-gradient(to_right,theme(colors.zinc.950)_0%,rgba(0,0,0,0)_100%)] 
+  before:bg-[linear-gradient(to_right,theme(colors.background)_0%,rgba(255,255,255,0)_100%)] 
+  dark:before:bg-[linear-gradient(to_right,theme(colors.background)_0%,rgba(0,0,0,0)_100%)] 
   before:content-[''] 
   after:absolute after:right-0 after:top-0 after:z-[2] after:h-full 
   md:after:w-[80px] after:w-[30px] 
   after:scale-x-[-1] 
-  after:bg-[linear-gradient(to_right,theme(colors.zinc.50)_0%,rgba(255,255,255,0)_100%)] 
-  dark:after:bg-[linear-gradient(to_right,theme(colors.zinc.950)_0%,rgba(0,0,0,0)_100%)] 
+  after:bg-[linear-gradient(to_right,theme(colors.background)_0%,rgba(255,255,255,0)_100%)] 
+  dark:after:bg-[linear-gradient(to_right,theme(colors.background)_0%,rgba(0,0,0,0)_100%)] 
   after:content-['']
     group    pt-1">
 			{["", ""].map((_, i) => (
