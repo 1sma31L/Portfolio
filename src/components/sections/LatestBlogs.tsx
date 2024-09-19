d69f1bab874c84061753412d9a19f0a14e0ad932
ismail <ismailismo78@gmail.com>
2024-09-20 00:35:08 +0100

    Refactor LatestBlogs component to add "See More" link

--- src/components/sections/LatestBlogs.tsx
+++ src/components/sections/LatestBlogs.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 
 import BlogCard from "../BlogCard";
+import Link from "next/link";
 import { Post } from "@/types/types";
 
 function LatestBlogs() {
@@ -10,7 +11,7 @@ function LatestBlogs() {
 	useEffect(() => {
 		const fetchPosts = async () => {
 			const response = await fetch("/api/posts", {
-				cache: "force-cache",
+				next: { revalidate: 60 * 60 * 12 },
 			} as RequestInit);
 			const data = await response.json();
 			setPosts(data);
@@ -20,13 +21,20 @@ function LatestBlogs() {
 	}, []);
 	return (
 		<section className="w-full flex flex-col">
-			<h1 className="text-3xl md:text-4xl font-bold mb-10 text-center md:text-left">
+			<h1 className="text-3xl md:text-4xl font-bold text-center md:text-left mb-10">
 				Latest Blog Posts
 			</h1>
 			<div className="flex flex-col gap-3">
 				{posts.map((post, i) => (
 					<BlogCard key={i} slug={post.slug} frontMatter={post.frontMatter} />
 				))}
+			</div>
+			<div className="self-end m-4">
+				<Link
+					href={"/blog"}
+					className="text-muted-foreground hover:text-foreground transition-all duration-300">
+					See More &gt;&gt;&gt;
+				</Link>
 			</div>
 		</section>
 	);
