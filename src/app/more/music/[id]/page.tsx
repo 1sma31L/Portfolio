7b42fd9580a1f45553b8a1ca823189dc02b62790
ismail <ismailismo78@gmail.com>
2024-09-28 02:56:05 +0100

    Refactor music page component and improve code structure

--- src/app/more/music/[id]/page.tsx
+++ src/app/more/music/[id]/page.tsx
@@ -4,29 +4,25 @@ import * as spotify from "@/lib/spotify";
 import AnimatedDiv from "@/components/AnimatedDiv";
 import BackButton from "@/components/BackButton";
 import { FaSpotify } from "react-icons/fa";
-import { IoIosMusicalNotes } from "react-icons/io";
 import Link from "next/link";
 import React from "react";
 
-async function page({
-	params,
-}: {
-	params: {
-		id: string;
-	};
-}) {
+// Async function for server-side fetching
+export default async function Page({ params }: { params: { id: string } }) {
 	const songs = await spotify.getPlaylistItems(params.id);
 	const playlistInfo = await spotify.getPlaylistInfo(params.id);
+
 	const playlistName = playlistInfo.name;
 	const playlistImage = playlistInfo.images[0].url;
 	const playlistID = playlistInfo.id;
+
 	return (
 		<AnimatedDiv id={22}>
-			<main className="container mx-auto min-h-[93vh] font-bold text-[24px] sm:text-[32px] py-6 px-4 sm:px-0 ">
+			<main className="container mx-auto min-h-[93vh] font-bold text-[24px] sm:text-[32px] py-6 px-4 sm:px-0">
 				<BackButton />
 				<img src={playlistImage} alt="" className="rounded-lg my-3" />
-				<h1 className={``}>{playlistName}</h1>
-				<div className="text-lg md:text-xl flex justify-start items-center mb-5 font-normal gap-2 group ">
+				<h1>{playlistName}</h1>
+				<div className="text-lg md:text-xl flex justify-start items-center mb-5 font-normal gap-2 group">
 					<Link
 						href={`https://open.spotify.com/playlist/${playlistID}`}
 						target="_blank"
@@ -50,8 +46,8 @@ async function page({
 								alt={song.track.name}
 								className="w-12"
 							/>
-							<div className="flex flex-col justify-evenly items-start ">
-								<h2 className="text-sm font-noraml">{song.track.name}</h2>
+							<div className="flex flex-col justify-evenly items-start">
+								<h2 className="text-sm font-normal">{song.track.name}</h2>
 								<p className="text-xs font-normal">
 									{song.track.artists[0].name}
 								</p>
@@ -63,5 +59,3 @@ async function page({
 		</AnimatedDiv>
 	);
 }
-
-export default page;
