993863a89bd2dfb97ab83254965bb2b4497ccbfb
ismail <ismailismo78@gmail.com>
2025-04-12 23:43:00 +0100

    refactor: streamline posts directory path for improved clarity and maintainability

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,3 @@
 import path from 'path';
-export const postsDirectory = path.join(
-  process.cwd(),
-  'src',
-  'content',
-  'posts'
-);
+export const postsDirectory = path.join(process.cwd(), 'src', 'content');
 export const themesDirectory = path.join(process.cwd(), 'src', 'themes');
